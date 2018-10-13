182695953ebaf0d39baae1387a368020594bb5b9
Louis012 <pennachiolouis@gmail.com>
2018-10-13 18:49:45 +0200

    Update constants.ts

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,9 +1,9 @@
 import { Player } from './../model/player';
 
-export var MIN_NUMBER_OF_MATCHES = 11;
-export var NUMBER_OF_MATCHES_VARIATIONS = 6;
-export var MAX_NUMBER_OF_MATCHES_REMOVED = 3;
-export var MAX_NUMBER_OF_MATCHES = MIN_NUMBER_OF_MATCHES + NUMBER_OF_MATCHES_VARIATIONS;
+export const MIN_NUMBER_OF_MATCHES = 11;
+export const NUMBER_OF_MATCHES_VARIATIONS = 6;
+export const MAX_NUMBER_OF_MATCHES_REMOVED = 3;
+export const MAX_NUMBER_OF_MATCHES = MIN_NUMBER_OF_MATCHES + NUMBER_OF_MATCHES_VARIATIONS;
 
 export const PLAYERS: Player[] = [
     {
